a36fe8a54eb9d0ea22776c324c8870b126de49bb
Emilio Pisanty <episanty@gmail.com>
2021-01-22 13:30:07 +0100

    Changed name for better clarity.

--- makepng.sh
+++ makepng.sh
@@ -3,10 +3,10 @@
 
 ### This shell script produces a png version of the package pdf file.
 
-pdftoppm build/authororcid.pdf authororcid-tmp -png -f 1 -l 1 -singlefile
+pdftoppm build/ORCIDinREVTeX.pdf ORCIDinREVTeX-tmp -png -f 1 -l 1 -singlefile
 
-convert -border 1 -bordercolor black authororcid-tmp.png authororcid.png 
+convert -border 1 -bordercolor black ORCIDinREVTeX-tmp.png ORCIDinREVTeX.png 
 
-rm authororcid-tmp.png
+rm ORCIDinREVTeX-tmp.png
 
 echo "done"
